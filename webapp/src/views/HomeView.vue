21b1ffddd5e4ec8633a025894c2f9d161f806547
Fluffy9 <Fluffy9@users.noreply.github.com>
2023-07-14 13:03:19 -0400

    initial

---
+++ webapp/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main class="container">
+    
+    <h1>RPC4</h1>
+    <p></p>
+
+  </main>
+</template>
